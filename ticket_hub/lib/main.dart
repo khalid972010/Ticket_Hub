4c9000ce6288bde0e72fe0fbe318f12388248105
Khalid Elsayed <khalid972010@gmail.com>
2024-04-21 20:13:14 +0200

    beginning home

--- ticket_hub/lib/main.dart
+++ ticket_hub/lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ticket_hub/firebase_options.dart';
 import 'package:ticket_hub/helpers/colors_helper.dart';
 import 'package:ticket_hub/screens/home/home.dart';
-import 'package:ticket_hub/screens/home_navigation/home_navigation.dart';
-import 'package:ticket_hub/screens/on_boarding/on_boarding.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- ticket_hub/lib/view_model/state_management/home_navigation/navigation_cubit.dart
+++ ticket_hub/lib/view_model/state_management/home_navigation/navigation_cubit.dart
@@ -1,6 +1,4 @@
 import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
 import 'package:ticket_hub/screens/favorites/favorites.dart';
 import 'package:ticket_hub/screens/home/home.dart';
 import 'package:ticket_hub/screens/my_tickets/my_tickets.dart';
