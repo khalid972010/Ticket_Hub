18c66d3dc4b27be87520434b8deb5c06e05bd5ec
SalahNageh <157980270+SalahNageh@users.noreply.github.com>
2024-04-22 00:08:10 +0200

    category card & Event Card

--- ticket_hub/lib/main.dart
+++ ticket_hub/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_hub/components/event_card_components.dart';
 import 'package:ticket_hub/firebase_options.dart';
 import 'package:ticket_hub/helpers/colors_helper.dart';
 import 'package:ticket_hub/screens/home/home.dart';
 import 'package:ticket_hub/screens/home_navigation/home_navigation.dart';
 import 'package:ticket_hub/screens/loading_screen/loading.dart';
+
+import 'components/category_card_components.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,7 +31,24 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: LoadingScreen()
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Categories",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        body: const EventCardComponent(),
+      ),
+      // Scaffold(
+      //   body: Container(
+      //       decoration: const BoxDecoration(
+      //           image: DecorationImage(
+      //         image: AssetImage("assets/images/main_background_light.png"),
+      //         fit: BoxFit.cover,
+      //       )),
+      //       child: HomeNavigation()),
+      // ),
     );
   }
 }
